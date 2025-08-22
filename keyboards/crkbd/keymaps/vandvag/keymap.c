a55274487da26161bc2ab0d8a70489205709f0f1
vandvag <vandvag@gmail.com>
2025-08-22 23:41:29 +0300

    [CRKBD]: Minor changes to symbols layout

--- keyboards/crkbd/keymaps/vandvag/keymap.c
+++ keyboards/crkbd/keymaps/vandvag/keymap.c
@@ -82,9 +82,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TAB,  KC_GRV, KC_TILDE, KC_HASH, KC_AMPR, KC_PIPE, KC_VOLU,    _______, KC_CIRC, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_EXLM, KC_UNDS, KC_COLN,  KC_EQL,  KC_DLR, KC_VOLD,    _______,   KC_AT, KC_LPRN, KC_RPRN, KC_UNDS, XXXXXXX, KC_PIPE,
+      KC_LCTL, KC_EXLM, KC_UNDS, KC_COLN,  KC_EQL, KC_SLSH, KC_VOLD,    _______, KC_PERC, KC_LPRN, KC_RPRN, KC_UNDS, _______, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_PERC, KC_SLSH, KC_ASTR, KC_PLUS, KC_BSLS,                      KC_SLSH, KC_MINS, KC_LABK, KC_RABK, XXXXXXX, KC_ESC,
+      KC_LSFT,   KC_AT, KC_SLSH, KC_ASTR, KC_PLUS, KC_BSLS,                       KC_DLR, KC_MINS, KC_LABK, KC_RABK, _______, KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
