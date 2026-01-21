c710baef088a4c8c9e50ce6ae19dd0bdb80e8b60
Evangelos Vandoros <vandvag@gmail.com>
2026-01-21 23:22:59 +0200

    [CRKBD]: Make master keyboard right
    Somehow there is an issue on my unit
    and the connection on the left-hand side is spotty...

--- keyboards/crkbd/keymaps/vandvag/config.h
+++ keyboards/crkbd/keymaps/vandvag/config.h
@@ -25,6 +25,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 200
 
+#define MASTER_RIGHT
+
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
