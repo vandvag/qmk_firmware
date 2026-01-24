5a97dc316361a74ffbb25052e54cc0d883035e35
Evangelos Vandoros <vandvag@gmail.com>
2026-01-24 13:10:35 +0200

    [SILAKKA]: Updated LCTL
    Left Control key is now escape if pressed once and Control when held.

--- keyboards/silakka54/keymaps/vandvag/keymap.c
+++ keyboards/silakka54/keymaps/vandvag/keymap.c
@@ -30,6 +30,8 @@
 #define FN_ALT  LT(_FN, KC_LALT)
 #define SFT_ENT LSFT_T(KC_ENT)
 
+#define CTL_ESC LCTL_T(KC_ESC)
+
 enum layers {
     _QWERTY,
     _COLEMAK,
@@ -46,14 +48,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
        XXXXXXX,    KC_1,     KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_GRV,
         KC_TAB,    KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-        KC_LCTL,  GUI_A,    ALT_S,   SFT_D,   CTL_F,    KC_G,                               KC_H,   CTL_J,   SFT_K,   ALT_L,GUI_SCLN,  KC_QUOT,
+       CTL_ESC,   GUI_A,    ALT_S,   SFT_D,   CTL_F,    KC_G,                               KC_H,   CTL_J,   SFT_K,   ALT_L,GUI_SCLN,  KC_QUOT,
         KC_LSFT,   KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_ESC,
                                            NUM_ALT, MO(_SYM), SFT_ENT,           KC_SPC,  MO(_NAV), FN_ALT
     ),
     [_COLEMAK] = LAYOUT(
        XXXXXXX,    KC_1,     KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_GRV,
         KC_TAB,    KC_Q,     KC_W,    KC_F,    KC_P,    KC_G,                               KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
-        KC_LCTL,  GUI_A,    ALT_R,   SFT_S,  CTL_TH,    KC_D,                               KC_H,   CTL_N,   SFT_E,   ALT_I,   GUI_O,  KC_QUOT,
+       CTL_ESC,   GUI_A,    ALT_R,   SFT_S,  CTL_TH,    KC_D,                               KC_H,   CTL_N,   SFT_E,   ALT_I,   GUI_O,  KC_QUOT,
         KC_LSFT,   KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,                               KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_ESC,
                                            NUM_ALT, MO(_SYM), SFT_ENT,           KC_SPC,  MO(_NAV), FN_ALT
     ),
